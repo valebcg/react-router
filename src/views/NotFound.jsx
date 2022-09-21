d5f808c345bad2d55fe95fef7b9febe14ebdb49a
valebcg <valeria.cortes.g@gmail.com>
2022-09-21 15:23:00 -0400

    fix

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 export default () => {
   return (
     <Container className="pt-5">
-      <h1 className="mb-4">La ruta que intentas consultar no existe :/</h1>
+      <h3 className="mb-4">La ruta que intentas consultar no existe :/</h3>
     </Container>
   );
 };
